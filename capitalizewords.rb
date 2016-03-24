b1badea352c84fb6fef91f0a3478867379a3e39b
Kevin Storberg <kevin@storberg.net>
2016-03-24 14:26:36 -0400

    capitalize words

--- capitalizewords.rb
+++ capitalizewords.rb
@@ -1,4 +1,13 @@
-File.open(ARGV[0]).each_line do |line|
-   words = line.split.each{|i| i.capitalize!}.join(' ')
-   puts words
+File.open('capitalizewords.txt').each_line do |line|
+   words = line.scan(/\S+/)
+   i = 0
+   string = ''
+   while i < words.length
+      word = words[i].split(//)
+      word[0] = word[0].capitalize
+      word = word.join('')
+      i = i + 1
+      string = string + word + ' '
+   end
+   puts string.chop
 end
