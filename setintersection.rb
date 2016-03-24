10276bb7a633eadaf479410eb981007df97a63ba
Kevin Storberg <kevin@storberg.net>
2016-03-24 12:41:09 -0400

    set Intersection

--- setintersection.rb
+++ setintersection.rb
@@ -1,16 +1,14 @@
-File.open(ARGV[0]).each_line do |line|
+File.open('setintersection.txt').each_line do |line|
    set_both = line.split(";")
    set_one = set_both[0].scan(/\d+/)
    set_two = set_both[1].scan(/\d+/)
+   intersection = ''
    i = 0
    while i < set_one.length
       j = 0
       while j < set_two.length
          if set_one[i] == set_two[j]
-            if j != 0
-               print ","
-            end
-            print set_one[i]
+            intersection = intersection + set_one[i] + ','
             j = j + 1
             i = i + 1
          else
@@ -19,5 +17,5 @@ File.open(ARGV[0]).each_line do |line|
       end
       i = i + 1
    end
-   puts
+   puts intersection.chop
 end
